86d2c27ec24037bd699206405fdf956e6949affb
John Cely <john.19col@gmail.com>
2025-10-30 23:46:20 -0700

    Expand description for all movies

--- src/components/poster-detail/PosterDetail.jsx
+++ src/components/poster-detail/PosterDetail.jsx
@@ -9,7 +9,11 @@ export const PosterDetail = ({ poster, onClose }) => {
       <img src={poster?.image} alt={poster?.title} />
       <div className="poster-detail-content">
         <h1>{poster?.title}</h1>
-        <p>{poster?.description}</p>
+        <div>
+          {poster?.description.split("\n").map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
         <button onClick={onClose}>Close</button>
       </div>
     </article>
